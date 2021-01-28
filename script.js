b3ce097c3c27a4a6fd1cad51f3cfce3141012807
danny-zero <sifuarrow@gmail.com>
2021-01-28 18:43:53 -0500

    adding drag and drop functionality

--- script.js
+++ script.js
@@ -11,32 +11,63 @@ const grid = document.querySelector('.grid');
 const width = Math.floor(grid.offsetWidth / 50)
 console.log(width)
 
-let counter = 0;
 const addRowBtn = document.querySelector('#addRow')
-addRowBtn.addEventListener("click", () => {
-    counter++
-    if (counter <= 100) {
-        for (let i = 0; i < width; i++) {
-            grid.innerHTML += `<div class="square gray"</div>`;
-        }
-    } else {
-        alert('Stop it!!!!!!')
-    }
+
+// let counter = 0;
+// addRowBtn.addEventListener("click", () => {
+//     counter++
+//     if (counter <= 100) {
+//         for (let i = 0; i < width; i++) {
+//             grid.innerHTML += `<div class="square gray"</div>`;
+//         }
+//     } else {
+//         alert('Stop it!!!!!!')
+//     }
     
+// })
+
+addRowBtn.addEventListener("click", () => {
+    for (let i = 0; i < width; i++) {
+        grid.innerHTML += `<div class="square gray"</div>`;
+    }  
 })
 
 const resetBtn = document.querySelector('#reset');
 resetBtn.addEventListener("click", () => {
-    location.reload();
+    // location.reload();
+    [...document.querySelectorAll('.square')].forEach((square) => {
+        square.style.backgroundColor = "gray"
+    });
 })
 
 
 
 const selectColor = document.querySelector("#selectColor");
-grid.addEventListener("click", (event) => {
-    console.log(selectColor.value);
-    if (event.target.classList.contains('square')) {
-        event.target.style.backgroundColor = selectColor.value;
-        event.target.classList.remove("gray")
-    }
+
+let mouseIsDown = false
+
+grid.addEventListener('mousedown', () => {
+  mouseIsDown = true
+  })
+grid.addEventListener('mouseup', () => {
+  mouseIsDown = false
+  })
+
+grid.addEventListener("mousemove", (event) => {
+  if (event.target.classList.contains('square') && mouseIsDown === true) {
+    event.target.style.backgroundColor = selectColor.value;
+  }
+})
+
+
+const reloadBtn = document.querySelector("#reload");
+reloadBtn.addEventListener("click", () => {
+    location.reload();
+})
+
+const removeBtn = document.querySelector("#remove");
+removeBtn.addEventListener("click", () => {
+    for (let i = 0; i < width; i++) {
+        grid.removeChild(grid.childNodes[grid.childNodes.length - 1]);
+    } 
 })
